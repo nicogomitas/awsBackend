a82349b3610c08dae43613f048fa374c555caca1
nicogomitas <163362246+nicogomitas@users.noreply.github.com>
2024-09-16 17:32:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,8 @@ CORS(app, resources={r"/*": {"origins": "http://ec2-18-205-177-229.compute-1.ama
 
 # Configuración de conexión a la base de datos
 def get_db_connection():
-    env = os.getenv('FLASK_ENV')
-    print(f"FLASK_ENV: {env}")  # Debugging line
-    if env == 'testing':
-        return sqlite3.connect(':memory:')  # Usar SQLite en memoria
+    if os.getenv('FLASK_ENV') == 'testing':
+        raise RuntimeError("In test environment, use a different script to handle database operations")
     else:
         return mysql.connector.connect(
             host='172.31.91.98',
@@ -61,3 +59,4 @@ def get_users():
 
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0', debug=True)
+
